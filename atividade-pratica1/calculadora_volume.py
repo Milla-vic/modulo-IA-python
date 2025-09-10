3ca025aa4995f7b6f564451595c745c4b2488883
Milla-vic <camilavpr80@gmail.com>
2025-09-10 13:36:17 -0300

    Concluído três novos exercícios

---
+++ atividade-pratica1/calculadora_volume.py
@@ -0,0 +1,12 @@
+# Este programa calcula o volume de caixa retangular
+
+# Solicita as dimensões da caixa ao usuario 
+comprimento = 12
+largura = 14 
+altura = 20 
+
+# Calcula o volume da caixa
+volume = comprimento * largura * altura
+
+# Exibe o resultado
+print(volume)
