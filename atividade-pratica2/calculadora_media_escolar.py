29e7220a31450ea258082805780785a5fd7290cc
Milla-vic <camilavpr80@gmail.com>
2025-09-17 11:17:41 -0300

    Calculo da média final do aluno

---
+++ atividade-pratica2/calculadora_media_escolar.py
@@ -0,0 +1,17 @@
+#Calculadora de média escolar
+
+#Notas do aluno
+nota1=input("Digite a primeira nota: ")
+nota2=input("Digite a segunda nota: ")
+nota3=input("Digite a terceira nota: ")
+
+#Calculo da média
+media= (nota1 + nota2 + nota3) / 3
+
+#Exibição do resultado
+print(f"Notas do aluno")
+print(f"Notas 1: {nota1}")
+print(f"Notas 2: {nota2}")
+print(f"Notas 3: {nota3}")
+print(f"Média final: {media:.2f}")
+
